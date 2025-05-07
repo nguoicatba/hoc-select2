2341611ab60627599af004e96581714c7ec177de
Nguyễn Lê Trung Kiên <thanheco1978@gmail.com>
2025-05-07 13:48:43 +0700

    test datatable

--- test_select2/wwwroot/js/testdatatable.js
+++ test_select2/wwwroot/js/testdatatable.js
@@ -1,15 +1,40 @@
 ﻿$(document).ready(function () {
     $('#example').DataTable({
-        "searching": false, 
+       
        
         "ajax": {
             "url": "/Home/GetDataTable",
             "type": "GET",
             "datatype": "json"
         },
+        "layout": {
+            "top2Start": 'search',
+          
+            "topStart": 'info',
+
+            "topEnd": function () {
+                return '<button class="btn btn-primary" id="btnExport">Export</button>';
+            },
+
+            "bottom": 'paging',
+            "bottomStart": null,
+            "bottomEnd": null,
+        },
 
         "columns": [
-            { "data": "id" },
+
+            {
+                "data": "id",
+                "render": function (data, type, row) {
+                    return '<input type="checkbox" class="select-checkbox" value="' + data.id + '">';
+                },
+                "orderable": false,
+                "searchable": false
+            },
+            {
+                "data": "id",
+                
+            },
             { "data": "name" },
             { "data": "email" },
             { "data": "phone" },
